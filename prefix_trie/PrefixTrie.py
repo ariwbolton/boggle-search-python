71b88e89eebe15efac5a99e723bbbc6b82bee681
Ari Wolff Bolton <ariwbolton@gmail.com>
2023-09-06 15:29:04 +0100

    Move some test code to live with PrefixTrie

--- prefix_trie/PrefixTrie.py
+++ prefix_trie/PrefixTrie.py
@@ -25,3 +25,24 @@ class PrefixTrie:
 
     def is_prefix(self, word: str):
         return self.root.is_prefix(word.lower())
+
+if __name__ == "__main__":
+    print('loading dictionary!')
+    dictionary = PrefixTrie.from_file("words")
+
+    print('loaded dictionary!')
+
+    print('is_word tests')
+
+    print(dictionary.is_word("aardvark"))
+    print(dictionary.is_word("nothing"))
+    print(dictionary.is_word("jsdlfjslkdjls"))
+    print(dictionary.is_word("Abencerrages"))
+
+    print('is_prefix tests')
+
+    print(dictionary.is_prefix("A"))
+    print(dictionary.is_prefix("Z"))
+    print(dictionary.is_prefix("Abelian"))
+    print(dictionary.is_prefix("Abel"))
+    print(dictionary.is_prefix("nonsensenotaword"))
