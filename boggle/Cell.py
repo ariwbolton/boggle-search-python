55a8fc4b86a6440e2db867ebc8f76300ff92c52a
Ari Wolff Bolton <ariwbolton@gmail.com>
2023-09-06 16:22:50 +0100

    Fix Cell neighbors_set

--- boggle/Cell.py
+++ boggle/Cell.py
@@ -7,15 +7,20 @@ class Cell:
         self.y = y
         self.letter = letter
         self.neighbors = []
+        self.neighbors_set = set()
 
     def add_neighbor(self, neighbor: 'Cell'):
         self.neighbors.append(neighbor)
+        self.neighbors_set.add(neighbor)
+
+    def is_neighbor(self, neighbor: 'Cell'):
+        return neighbor in self.neighbors_set
 
     def vec(self) -> Tuple[int, int]:
         return self.x, self.y
 
     def __hash__(self):
-        return self.vec()
+        return hash(self.vec())
 
     def __repr__(self):
         return str(str(self.vec()) + ": " + self.letter)
